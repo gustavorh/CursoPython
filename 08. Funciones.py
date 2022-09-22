36707448c79162610b3e37fe6ae9557546a3618a
Gustavo <gustavocars@gmail.com>
2022-09-22 18:51:33 -0300

    Update 08. Funciones.py

--- 08. Funciones.py
+++ 08. Funciones.py
@@ -11,4 +11,4 @@ def conversorMoneda(valorDolar):
     pesos = int(input("Ingrese un valor en pesos chilenos (CLP): "))
     dolares = pesos / valorDolar
     dolares = round(dolares, 2)
-    print(str(dolares) + " dolares")
+    print(str(dolares) + " dolares") 
